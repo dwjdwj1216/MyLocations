54d556759dc55accf5b2d8665ac56af0e3bb328e
dwjdwj1216 <dwjdwj2@126.com>
2015-08-11 17:41:15 +0800

    Initial Commit

---
+++ MyLocations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyLocations
+//
+//  Created by 戴王炯 on 15/8/11.
+//  Copyright © 2015年 戴王炯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyLocations/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  MyLocations
+//
+//  Created by 戴王炯 on 15/8/11.
+//  Copyright © 2015年 戴王炯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
